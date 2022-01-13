29b8f5367f71e98db5200885c7a374281b5d2b3f
DanielDlc <dlc.engcomputacao@gmail.com>
2022-01-13 09:34:22 -0300

    criando heroes

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolverine' },
+  { id: 2, name: 'Homem Aranha' },
+  { id: 3, name: 'Doutor Estranho' },
+  { id: 4, name: 'Magneto' },
+  { id: 5, name: 'Hulk' },
+  { id: 6, name: 'Thor' },
+  { id: 7, name: 'Psyloke' },
+  { id: 8, name: 'Massacre' },
+  { id: 9, name: 'Sentinela' },
+  { id: 10, name: 'Dente de Sabre' },
+];
